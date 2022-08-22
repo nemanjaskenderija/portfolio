2b2c9d90b543cafb8c87bf5887a9a75fa675e559
nemanjaskenderija <97840854+nemanjaskenderija@users.noreply.github.com>
2022-08-22 11:31:43 +0200

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: 'projects', component: ProjectsComponent },
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
